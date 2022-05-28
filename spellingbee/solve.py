b87cec5871d3cd22af6b4bb985b732b7228669aa
Charles Jaimet <cjaimet@postmedia.com>
2022-05-27 17:03:46 -0700

    Eliminate one regex

--- spellingbee/solve.py
+++ spellingbee/solve.py
@@ -5,7 +5,6 @@ def get_letters():
     if 1 < len(sys.argv):
         output = sys.argv[1]
     if ( 7 == len( output ) ):
-        print( output[6] )
         qs = re.search( r"^([A-Z]{7})$", output )
         if qs:
             return output.lower()
@@ -26,36 +25,23 @@ def clean_words( words ):
             output.append( word )
     return output
 
-def get_words_by_primary_letter( words, letter ):
-    output = []
-    rule = r".*"+letter+".*"
-    for word in words:
-        if ( re.match( rule, word ) ):
-            output.append( word )
-    return output
-
 def get_words_by_all_letters( words, letters ):
     output = []
-    rule = r"^["+letters+"]+$"
+    rule = r"^["+letters+"]*"+letters[0]+"["+letters+"]*$"
     for word in words:
         if ( re.match( rule, word ) ):
             output.append( word )
+    output.sort()
     return output
 
+
+
+
+
 letters = get_letters()
-print(letters)
-print(letters[0])
 words = get_words_from_file( './data/words.txt' )
 words = clean_words( words )
-words = get_words_by_primary_letter( words, letters[0] )
 words = get_words_by_all_letters( words, letters )
+
 for word in words:
     print( word )
-quit()
-
-rule = "aa[a-z]{10}"
-
-with open( './data/words.txt' ) as words:
-    for word in words:
-        if ( re.match( rule, word, re.IGNORECASE ) ):
-            print( word.replace( "\n", '' ) )
